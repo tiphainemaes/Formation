cbdd9ed80c40803b4a1014fb9708cee37930b55f
tiphaine <tiphaine.maes@gmail.com>
2020-04-09 10:48:31 +0200

    modification exercice 11

--- 02-HTML-CSS/06-exercice-html5/exercice-11/exercice-11.js
+++ 02-HTML-CSS/06-exercice-html5/exercice-11/exercice-11.js
@@ -1,50 +1,35 @@
-// 1.Ecrire un script qui permet d'afficher la table de multiplication d'un nombre
-// entier n:
-// - La valeur de n sera demandée à l’utilisateur via une boîte de dialogue.
-// - En cas de saisie d'un nombre non entier le script redemande
-// l'utilisateur de saisir une nouvelle valeur.
-// - L'affichage de résultat doit être encadré par un tableau html.
-
-// 2. Utiliser les boucles imbriquées pour afficher l'ensemble des tables de
-// multiplication pour n=1,2.....10.
-
-
 function demanderNombre() {
+    let n = parseInt(document.getElementById("saisie").value);
+    let tab = document.getElementById("tab-n");
+    let produit = 0;
 
-    let n = prompt("Entre un entier n :");
-    while (n.includes(",") || n.includes(".")) {
-        n = prompt("Entre un entier n :");
-    }
-
-    document.write("<table>");
-    document.write("<tr>");
-    document.write("<th>n=</th><th>" + n + "</th>");
-    document.write("</tr>");
     for (i = 1; i <= 10; i++) {
+        let row = tab.insertRow(i);
+        let celli = row.insertCell(0);
+        let celln = row.insertCell(1);
         produit = n * i;
-        document.write("<tr>");
-        document.write("<td>" + i + "</td>");
-        document.write("<td>" + produit + "</td>");
-        document.write("</tr>");
+
+        celli.innerHTML = i + " x " + n + " = ";
+        celln.innerHTML = produit;
     }
-    document.write("</table>");
 }
 
-
 function table() {
-    document.write("<table>");
-
-    let n = 0;
+    let n = 1;
+    let produit = 0;
+    let tabMulti = document.getElementById("tab-multi");
+    let row;
+    let cell;
 
     for (i = 1; i <= 10; i++) {
         produit = n * i;
-        document.write("<tr>");
+        row = tabMulti.insertRow(i);
+        cell = row.insertCell(0);
 
         for (n = 1; n <= 10; n++) {
             produit = n * i;
-            document.write("<td>" + produit + "</td>");
+            cell = row.insertCell(n);
+            cell.innerHTML = produit;
         }
-        document.write("</tr>");
     }
-    document.write("</table>");
 }
