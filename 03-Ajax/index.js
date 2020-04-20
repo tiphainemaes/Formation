fa7ef92fed64a8dfe83b347e5f055c6b11510552
tiphaine <tiphaine.maes@gmail.com>
2020-04-20 12:54:25 +0200

    exercice ajax fini

--- 03-Ajax/index.js
+++ 03-Ajax/index.js
@@ -1,4 +1,9 @@
 $('#button-users').click(function() {
+
+    if (!($('#list-users').is(':empty'))) {
+        $("#list-users").empty();
+    }
+
     $.get("http://localhost:4000/users", function(
         users,
         status
@@ -17,10 +22,19 @@ $('#button-users').click(function() {
                             users,
                             status) {
                             const divUserPosts = $('#user-posts');
-                            const userPost = $('<h1/>');
+                            const usernamePost = $('<h1/>');
+                            const userPost = $('<h4/>');
+
+                            usernamePost.append(user.username);
                             userPost.append(user.name);
+                            divUserPosts.append(usernamePost);
                             divUserPosts.append(userPost);
                         })
+
+                    if (!($('#list-posts').is(':empty'))) {
+                        $("#user-posts").empty();
+                        $("#list-posts").empty();
+                    }
 
                     $.get("http://localhost:4000/posts",
                         function(
@@ -33,33 +47,41 @@ $('#button-users').click(function() {
                                 if (post.userId == user.id) {
                                     const h3 = $('<h3/>');
                                     const p = $('<p/>');
-                                    const divComment = $('<div/>')
+                                    const divComment = $('<div/>', {
+                                        id: "test"
+                                    });
+
                                     const btnComments = $('<button/>', {
                                         text: 'Comments',
                                         id: post.id,
                                         click: function() {
-                                            $.get("http://localhost:4000/comments",
-                                                function(
-                                                    comments,
-                                                    status
-                                                ) {
-                                                    comments.forEach(comment => {
 
-                                                        if (comment.postId == post.id) {
-                                                            const nameComment = $('<h6/>');
-                                                            const emailComment = $('<p/>');
-                                                            const bodyComment = $('<p/>');
+                                            if (!($(divComment).is(':empty'))) {
+                                                $(divComment).empty();
+                                            } else {
+                                                $.get("http://localhost:4000/comments",
+                                                    function(
+                                                        comments,
+                                                        status
+                                                    ) {
+                                                        comments.forEach(comment => {
 
-                                                            nameComment.append(comment.name);
-                                                            emailComment.append('by ' + comment.email);
-                                                            bodyComment.append(comment.body);
+                                                            if (comment.postId == post.id) {
+                                                                const nameComment = $('<h6/>');
+                                                                const emailComment = $('<p/>');
+                                                                const bodyComment = $('<p/>');
 
-                                                            divComment.append(nameComment);
-                                                            divComment.append(emailComment)
-                                                            divComment.append(bodyComment);
-                                                        }
+                                                                nameComment.append(comment.name);
+                                                                emailComment.append('by ' + comment.email);
+                                                                bodyComment.append(comment.body);
+
+                                                                divComment.append(nameComment);
+                                                                divComment.append(emailComment)
+                                                                divComment.append(bodyComment);
+                                                            }
+                                                        });
                                                     });
-                                                });
+                                            }
                                         }
                                     });
 
@@ -69,7 +91,14 @@ $('#button-users').click(function() {
                                     divListPosts.append(h3);
                                     divListPosts.append(p);
                                     divListPosts.append(btnComments);
-                                    divListPosts.append(divComment)
+                                    divListPosts.append(divComment);
+
+
+                                    $('#refresh').click(function() {
+                                        $("#user-posts").empty();
+                                        $("#list-posts").empty();
+                                    });
+
 
                                 }
                             });
@@ -79,4 +108,5 @@ $('#button-users').click(function() {
             divListUser.append(btn);
         });
     });
+
 });
