9b6a4ac3a97da56916d101d88c84d957c5108a95
tiphaine <tiphaine.maes@gmail.com>
2020-05-13 12:58:53 +0200

    quest 3 sql

--- 05-Postgres/database.sql
+++ 05-Postgres/database.sql
@@ -1,16 +1,39 @@
---14. Supprimer toutes les commandes (et les lignes des commandes) inférieur au 
---1er février 2019. Cela doit être effectué en 2 requêtes maximum
+-- 8. Peut-on savoir combien d'utilisateurs distincts ont loué chaque genre ?
+ -- SELECT * FROM actor;
+-- SELECT * FROM address;
+-- SELECT * FROM category;
+-- SELECT * FROM city;
+-- SELECT * FROM country;
+-- SELECT * FROM customer;
+-- SELECT * FROM film;
+-- SELECT * FROM film_actor;
+-- SELECT * FROM film_category;
+-- SELECT * FROM inventory;
+-- SELECT * FROM language;
+-- SELECT * FROM payment;
+-- SELECT * FROM rental;
+-- SELECT * FROM staff;
+-- SELECT * FROM store;
 
-SELECT nom, COUNT(nom) AS nombre_produit, 
-FROM commande_ligne
-GROUP BY nom
-HAVING COUNT(nom) > 1
-ORDER BY nombre_produit DESC;
+SELECT category.name,
+       SUM(payment.payment.amount) AS total_sales,
+       SUM(payment_amount) / 16 AS moyenne
+FROM payment
+INNER JOIN rental ON payment.rental_id = rental.rental_id
+INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+INNER JOIN film_category ON inventory.film_id = film_category.film_id
+INNER JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name;
 
 
-SELECT * FROM client;
-SELECT * FROM commande;
-SELECT * FROM commande_ligne;
+SELECT *
+FROM category;
 
 
+SELECT *
+FROM film_category;
+
+
+SELECT *
+FROM payment;
 
